d3227c0c80b9d0ee5bb4b2115730261e27e685a2
rexmeyer23 <rex_meyer_1426@yahoo.com>
2022-01-18 19:37:24 -0500

    notelistitem added

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize] //makes views accessible only to logged in users
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+        // In the code above, we are initializing a new instance of the NoteListItem as an IEnumerable with the [0] syntax. This will satisfy some of the requirements for our Index View. When we added the List template for our view, it created some IEnumerable requirements for our list view. 
+    }
+}
