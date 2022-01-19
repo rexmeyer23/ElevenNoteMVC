6a66ff9b2efae0f7e28d3c01533e64c13ad0a8ae
rexmeyer23 <rex_meyer_1426@yahoo.com>
2022-01-19 02:40:04 -0500

    more isstarred added

--- ElevenNote.WebMVC/Global.asax.cs
+++ ElevenNote.WebMVC/Global.asax.cs
@@ -14,6 +14,7 @@ namespace ElevenNote.WebMVC
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
